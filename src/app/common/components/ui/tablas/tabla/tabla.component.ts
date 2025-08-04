d9dcb6d4435480cade696f084a23db74435c17e3
sebastian <sebastian.h.piedrahita@gmail.com>
2025-08-04 16:56:29 -0500

    feat: mejoras tabla

--- src/app/common/components/ui/tablas/tabla/tabla.component.ts
+++ src/app/common/components/ui/tablas/tabla/tabla.component.ts
@@ -8,6 +8,7 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './tabla.component.html',
+  styleUrls: ['./tabla.component.scss'],
 })
 export class TablaComponent {
   // Propiedades de entrada
