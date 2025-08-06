61b31af10f47033adde8226934171ea0c56dcfaf
sebastian <sebastian.h.piedrahita@gmail.com>
2025-08-06 13:24:54 -0500

    feat: integrar filtros en conductor lista

---
+++ src/app/modules/conductor/mapeo/conductor-filtros.mapeo.ts
@@ -0,0 +1,9 @@
+import { FilterField } from '@app/common/components/ui/filtro/interface/filtro.interface';
+
+export const CONDUCTOR_LISTA_FILTERS: FilterField[] = [
+  { name: 'id', displayName: 'ID', type: 'number' },
+  { name: 'identificacion', displayName: 'Identificación', type: 'string' },
+  { name: 'nombre', displayName: 'Nombre', type: 'string' },
+  // Ejemplo de campo de relación (descomentarizar si es necesario):
+  // { name: 'empresa', displayName: 'Empresa', type: 'relation', relationEndpoint: 'transporte/empresa/' },
+];
--- src/app/modules/conductor/paginas/conductor-lista/conductor-lista.component.ts
+++ src/app/modules/conductor/paginas/conductor-lista/conductor-lista.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FiltroComponent } from '@app/common/components/ui/filtro/filtro.component';
 import { PaginadorComponent } from '@app/common/components/ui/paginador/paginador.component';
 import { TablaComponent } from '@app/common/components/ui/tablas/tabla/tabla.component';
 import { EstadoPaginacion } from '@app/common/interfaces/paginacion.interface';
@@ -7,21 +8,24 @@ import { QueryParams } from '@app/core/interfaces/api.interface';
 import { forkJoin } from 'rxjs';
 import { Conductor } from './../../interfaces/conductor.interface';
 import { columnasConductorLista } from '../../mapeo/conductor-lista.mapeo';
+import { CONDUCTOR_LISTA_FILTERS } from '../../mapeo/conductor-filtros.mapeo';
 import { ConductorRepository } from '../../repository/conductor.repository';
 
 @Component({
   selector: 'app-conductor-lista',
   standalone: true,
-  imports: [RouterModule, TablaComponent, PaginadorComponent],
+  imports: [RouterModule, TablaComponent, PaginadorComponent, FiltroComponent],
   templateUrl: './conductor-lista.component.html',
   styleUrl: './conductor-lista.component.scss',
 })
 export default class ConductorListaComponent implements OnInit {
   private _conductorRepository = inject(ConductorRepository);
+  private _filtrosActivos = signal<QueryParams>({});
 
   public conductoresSeleccionados = signal<Conductor[]>([]);
   public conductores = signal<Conductor[]>([]);
   public columnas = columnasConductorLista;
+  public camposFiltros = CONDUCTOR_LISTA_FILTERS;
   public estadoPaginacion = signal<EstadoPaginacion>({
     paginaActual: 1,
     itemsPorPagina: 30,
@@ -35,6 +39,8 @@ export default class ConductorListaComponent implements OnInit {
   consultarInformacion() {
     const parametros: QueryParams = {
       page: this.estadoPaginacion().paginaActual,
+      page_size: this.estadoPaginacion().itemsPorPagina,
+      ...this._filtrosActivos(),
     };
 
     this._conductorRepository.lista(parametros).subscribe(respuesta => {
@@ -54,6 +60,15 @@ export default class ConductorListaComponent implements OnInit {
 
   onSeleccionConductores(conductores: Conductor[]) {
     this.conductoresSeleccionados.set(conductores);
+  }
+
+  onFiltersChange(filtros: QueryParams): void {
+    this._filtrosActivos.set(filtros);
+    this.estadoPaginacion.update(estado => ({
+      ...estado,
+      paginaActual: 1,
+    }));
+    this.consultarInformacion();
   }
 
   eliminar() {
